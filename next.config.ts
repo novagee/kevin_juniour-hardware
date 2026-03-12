88c0438bb33ce5d4144f16bac675b051318b1619
Gideonngige <ushindigideon01@gmail.com>
2026-03-12 09:08:19 +0300

    chnaged to live backend url

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
+        destination: 'https://kevinjuniour-hardwarebackend.vercel.app/api/:path*',
       },
     ];
   },
